5ffd15e517ce10bd80e10bdcd972cec104f91565
Ritwik Ghosh <76449145+ritwik-ritnov@users.noreply.github.com>
2026-08-09 11:44:31 +0530

    Add files via upload

---
+++ Srijit/31.py
@@ -0,0 +1,7 @@
+import turtle
+for steps in range(1000):
+    for c in ('blue', 'red', 'green'):
+        turtle.color(c)
+        turtle.forward(steps)
+        turtle. right(30)
+    
---
+++ Srijit/32.py
@@ -0,0 +1,6 @@
+l=int(input("input length:"))
+b=int(input("input breadth:"))
+p=2*(l+b)
+a=l*b
+print("parameter=",p)
+print("area=",a)
